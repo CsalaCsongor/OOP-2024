0c7a90703b969ef48c41cdd8ba417d18ae1e36cf
CsalaCsongor <csala.csongor@student.ms.sapientia.ro>
2024-03-04 15:44:23 +0100

    Initial commit

--- labor-03/src/oop/labor03/Main.java
+++ labor-03/src/oop/labor03/Main.java
@@ -1,12 +1,16 @@
 package oop.labor03;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("labor03");
-    }
-    {
-        Rectangle rectangle = new Rectangle(10,10);
+    public static void main(String[] args)
 
+    {
+        Rectangle rectangle = new Rectangle(10, 10, 10, 10);
+        if(rectangle.isSquare())
+        {
+            System.out.println("Hello");
+        }
+
+    }
 
 
 }
